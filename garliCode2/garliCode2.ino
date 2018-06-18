c102eadfe68eb69d39e540eae9e1c73661387f0e
mathSlug <johnsumnerbentley@gmail.com>
2018-06-17 21:15:26 -0700

    Cleaned up comments

--- garliCode2/garliCode2.ino
+++ garliCode2/garliCode2.ino
@@ -1,8 +1,21 @@
+/*Controls Garlic Bread robot based on serial input
+ * Char '1': Turns on the hot plate 
+ * 2: Turns off hot plate
+ * 3: Turns the servo to its limit in one direction
+ * 4: Turns the servo to the limit in the opposite direction
+ * 5: Centers the servo
+ * 6: Turns on the LED - ideally an indicator when the bread is finished and must be replace
+ * 7: Resets - servo to center, LED and hot plate off - send before replacing bread
+ * all else: nothing occurs
+ * 
+ * Apologies for the hacky code
+ */
+
 
 
 int hotPadPin = 11;
 int servoPin = 7;
-char val;
+char val; // holds character received from serial
 int ledPin = 5;
 int smallLedPin = 13;
 int smallLedOn = 0;
@@ -11,14 +24,14 @@ int smallLedOn = 0;
 #include <Servo.h> 
  
 Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
+                
  
 int pos = 0;    // variable to store the servo position 
 int lastPos = pos;
  
 void setup() 
 { 
-  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(servoPin);  // attaches the servo on pin 7 to the servo object 
   pinMode(ledPin, OUTPUT);
   pinMode(hotPadPin, OUTPUT);
   pinMode(smallLedPin, OUTPUT);
@@ -29,7 +42,8 @@ void setup()
 void loop() 
 { 
   if (Serial.available()) {
-    val = Serial.read();
+    val = Serial.read(); // continually read Serial. If no new serial data is sent, the previous byte's command is executed
+                         // This is fine. Servo will stay at commanded position, LED and hot plate state will remain
   }
   
   if (val == '1') {
@@ -43,37 +57,37 @@ void loop()
     
   } else if (val == '3' && lastPos > 170) {
   
-    for(pos = lastPos; pos < 180; pos += 1)  // goes from 180- degrees to 180 degrees 
-     {                                  // in steps of 1 degree 
-       myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-       delay(15);                       // waits 15ms for the servo to reach the position 
+    for(pos = lastPos; pos < 180; pos += 1)   // smoothly move servo, easier on robot structure
+     {                                  
+       myservo.write(pos);              
+       delay(15);                // waits 15ms for the servo to reach the position. reduces jitter        
      }
      lastPos = pos; 
   
   } else if (val == '4' && lastPos < 10) {
   
-     for(pos = lastPos; pos>=1; pos-=1)     // goes from 0+ degrees to 0 degrees 
+     for(pos = lastPos; pos>=1; pos-=1)     
      {                                
-       myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-       delay(15);                       // waits 15ms for the servo to reach the position 
+       myservo.write(pos);              
+       delay(15);                       
      }
      lastPos = pos; 
      
   } else if (val == '5' && lastPos > 80 && lastPos < 100) {
   
     if (lastPos > 90) {
-      for(pos = lastPos; pos>=90; pos-=1)     // goes from 90+ degrees to 90 degrees 
+      for(pos = lastPos; pos>=90; pos-=1)    
       {                                
-       myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-       delay(15);                       // waits 15ms for the servo to reach the position 
+       myservo.write(pos);               
+       delay(15);                     
       }
       lastPos = pos;
       
     } else if (lastPos < 90) {
-      for(pos = lastPos; pos>90; pos+=1)     // goes from 90- degrees to 90 degrees 
+      for(pos = lastPos; pos>90; pos+=1)    
       {                                
-       myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-       delay(15);                       // waits 15ms for the servo to reach the position 
+       myservo.write(pos);              
+       delay(15);                       
       }
       lastPos = pos;
     }
@@ -89,22 +103,22 @@ void loop()
     smallLedOn = 0;
    
     if (lastPos > 90) {
-      for(pos = lastPos; pos>=90; pos-=1)     // goes from 90+ degrees to 90 degrees 
+      for(pos = lastPos; pos>=90; pos-=1)     
       {                                
-       myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-       delay(15);                       // waits 15ms for the servo to reach the position 
+       myservo.write(pos);              
+       delay(15);                       
       }
       lastPos = 90;
     } else if (lastPos < 90) {
-      for(pos = lastPos; pos>90; pos+=1)     // goes from 90- degrees to 90 degrees 
+      for(pos = lastPos; pos>90; pos+=1)    
       {                                
-       myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-       delay(15);                       // waits 15ms for the servo to reach the position 
+       myservo.write(pos);              
+       delay(15);                       
       }
       lastPos = 90;
     }
  
- // when there's not signal from processing
+ // when there's not a serial signal
  } else {
  
    if (smallLedOn == 0) {
